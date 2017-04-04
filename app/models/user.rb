8a090a7d8985cdc6862706616a7a34cc4825e13c
klocode <kendrick.t.lo@gmail.com>
2017-04-03 21:13:40 -0400

    routes and some db edits

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ApplicationRecord
 
   has_many :taggings, dependent: :destroy
   has_many :tags, through: :taggings
+  has_many :ratings
 
   has_secure_password
   has_secure_token :token
