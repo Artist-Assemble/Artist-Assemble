db4045554defa1aead1e5c5a0cefbce34fa5face
Gabe Wright <gabe1331@gmail.com>
2017-04-04 15:55:00 -0400

    90% singup layout

--- app-react/src/components/HeaderSub.js
+++ app-react/src/components/HeaderSub.js
@@ -3,11 +3,23 @@ import { browserHistory } from 'react-router'
 
 class HeaderSub extends React.Component {
     render() {
-        return <div className="container">
-            <div className="columns has-text-centered">
-                <div className="column is-12">
-                    <img src="img/logo.png" alt="artist assemble logo"/>
+        return <div className="container my-container hdr-sub">
+            <div className="columns is-mobile">
+                <div className="column is-4">
+                    <img src="img/logo.png" alt="artist assemble logo" className="hdr-img-sub"/>
                 </div>
+                <div className="column is-2"></div>
+                <div className="column is-2">
+                    <i className="fa fa-user fa-2x user" aria-hidden="true"></i>
+                </div>
+                <div className="column is-2">
+                    <p className="search-artists is-hidden-mobile"><i className="fa fa-search" aria-hidden="true"></i> artists</p>
+                    <p className="search-artists-mobile is-hidden-tablet"><i className="fa fa-search fa-2x" aria-hidden="true"></i></p>
+                </div>
+                <div className="column is-2">
+                    <a className="button is-small logout" onClick={() => browserHistory.push('/')}>logout</a>
+                </div>
+
             </div>
         </div>
         
--- app-react/src/components/Signup.js
+++ app-react/src/components/Signup.js
@@ -1,14 +1,94 @@
 import React from 'react'
 // import { browserHistory } from 'react-router'
-import HeaderSub from './HeaderSub'
+import HeaderMin from './HeaderMin'
 
 class Signup extends React.Component {
     render() {
         return <div>
-        <header>
-            <HeaderSub/>
-        </header>
-            <h1>Signup</h1>
+            <header>
+                <HeaderMin/>
+            </header>
+            <div className="lines">
+                <section className="container field-container">
+                    <div className="columns has-text-centered is-mobile">
+                        <div className="column is-offset-2 is-8 signup-fields">
+                            <div className="field">
+                                <label className="label" id="name">name</label>
+                                <p className="control">
+                                    <input className="input" type="text"/>
+                                </p>
+                            </div>
+                            <div className="field">
+                                <label className="label" id="email">email</label>
+                                <p className="control">
+                                    <input className="input" type="text"/>
+                                </p>
+                            </div>
+                            <div className="field">
+                                <label className="label" id="password">password</label>
+                                <p className="control">
+                                    <input className="input" type="text"/>
+                                </p>
+                            </div>
+                            <div className="field">
+                                <label className="label" id="re-password">retype password</label>
+                                <p className="control">
+                                    <input className="input" type="text"/>
+                                </p>
+                            </div>
+                            <div className="field">
+                                <label className="label">Bio</label>
+                                <p className="control">
+                                    <textarea className="textarea" placeholder="In one or two sentences, tell others about yourself and your music!"></textarea>
+                                </p>
+                            </div>
+                            <div className="field">
+                                <label className="label">I'm a..</label>
+                                <div className="checkboxes">
+                                    <p className="control">
+                                    <label className="checkbox">
+                                        <input type="checkbox"/>
+                                        singer/songwriter
+                                    </label>
+                                    </p>
+                                    <p className="control">
+                                        <label className="checkbox">
+                                            <input type="checkbox"/>
+                                            producer
+                                        </label>
+                                    </p>
+                                    <p className="control">
+                                        <label className="checkbox">
+                                            <input type="checkbox"/>
+                                            mix &amp; or mastering engineer
+                                        </label>
+                                    </p>
+                                </div>
+                            </div>
+                            <div className="field has-text-centered genre-drop">
+                                 <label className="label" id="name">my genre</label>
+                                <p className="control">
+                                    <span className="select">
+                                    <select className="options">
+                                        <option>edm</option>
+                                        <option>electronic</option>
+                                        <option>rap/hip-hop</option>
+                                        <option>pop</option>
+                                        <option>experimental</option>
+                                        <option>ambient</option>
+                                        <option>Indie</option>
+                                        <option>other</option>
+                                    </select>
+                                    </span>
+                                </p>
+                            </div>
+                            <a className="signup-next">
+                                next <i className="fa fa-step-forward" aria-hidden="true"></i>
+                            </a>
+                        </div>
+                    </div>
+                </section>
+            </div>
         </div>
     }
 }
