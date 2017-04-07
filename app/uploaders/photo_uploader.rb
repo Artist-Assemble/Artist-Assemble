8cd0deeeb3bb49c4586713791274b05d4a374f2e
klocode <kendrick.t.lo@gmail.com>
2017-04-07 13:49:28 -0400

    added uploaders and pusher gem

--- app/uploaders/photo_uploader.rb
+++ app/uploaders/photo_uploader.rb
@@ -24,7 +24,7 @@ class PhotoUploader < CarrierWave::Uploader::Base
 
   # Process files as they are uploaded:
   # process scale: [200, 300]
-  process resize_to_fit: [200, 200]
+  process resize_to_fit: [600, 600]
 
   #
   # def scale(width, height)
--- test/factories/collaborations.rb
+++
@@ -1,5 +0,0 @@
-FactoryGirl.define do
-  factory :collaboration do
-    status "MyString"
-  end
-end
