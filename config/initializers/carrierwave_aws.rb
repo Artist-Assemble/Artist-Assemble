c2ff8615c9d1e5439408e80e4f186ae2a0b283ce
klocode <kendrick.t.lo@gmail.com>
2017-04-10 14:00:58 -0400

    setting region for AWS

--- config/initializers/carrierwave_aws.rb
+++ config/initializers/carrierwave_aws.rb
@@ -3,7 +3,8 @@ CarrierWave.configure do |config|
   config.fog_credentials = {
     provider:              'AWS',                        # required
     aws_access_key_id:     ENV['aws_key'],                        # required
-    aws_secret_access_key: ENV['aws_secret']                        # required
+    aws_secret_access_key: ENV['aws_secret'],                        # required
+    region:                'us-east-2'
   }
   config.fog_directory  = 'artist-assemble'                          # required
 end
