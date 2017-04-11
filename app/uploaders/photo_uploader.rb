255ea20fe3057565599ecdb986d6483272f84aa4
klocode <kendrick.t.lo@gmail.com>
2017-04-11 10:45:40 -0400

    trying to get square

--- app/uploaders/photo_uploader.rb
+++ app/uploaders/photo_uploader.rb
@@ -24,7 +24,7 @@ class PhotoUploader < CarrierWave::Uploader::Base
 
   # Process files as they are uploaded:
   # process scale: [200, 300]
-  process resize_to_fit: [600, 600]
+  process resize_to_fill: [600, 600]
 
   #
   # def scale(width, height)
