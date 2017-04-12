4f0ebb846a2e4634a254b5e4ae77e6148385d120
klocode <kendrick.t.lo@gmail.com>
2017-04-12 14:38:05 -0400

    adding more stuff to collab serializer

--- app/models/collaboration.rb
+++ app/models/collaboration.rb
@@ -2,7 +2,10 @@ class Collaboration < ApplicationRecord
   belongs_to :user
   belongs_to :collaborator, class_name: "User"
 
+  has_many :messages
+  
   validates :collaborator, :status, presence: true
+
 
 
 
