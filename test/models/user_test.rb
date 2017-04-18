257600b882078e07d8567601ad432052a8d89829
klocode <kendrick.t.lo@gmail.com>
2017-04-18 11:29:46 -0400

    adding tests

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,10 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  should have_secure_password
+  should have_many :tags
+  should have_many :taggings
+  should have_many :ratings
+  should have_many :collaborations
+  should have_many :messages
 end
